ce59bf9e9f3de9ac122eadf71180e4ece807a0ea
takei.shg <takei.shg@gmail.com>
2017-07-18 19:02:21 +0900

    Add provision.sh

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+sudo yum update -y
+sudo yum -y install bind bind-utils
+
+# set bind files
+sudo cp /etc/named.conf /etc/named.conf.ori
+sudo cp /vagrant/aiit_network_lec/cfg_files/bind/named.conf /etc/named.conf
+sudo chown root:named /etc/named.conf
+sudo cp /vagrant/aiit_network_lec/cfg_files/bind/zones/* /var/named/
+sudo chown root:named /var/named/net06.*
+sudo /etc/init.d/named restart
+
