814589b72ce22447795dc1d1b80bff4ac6f5968b
takei.shg <takei.shg@gmail.com>
2017-07-18 20:21:41 +0900

    Mod backup file name

--- provision.sh
+++ provision.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+now=$(date +"%Y%m%d_%H_%M_%S")
+
 sudo yum update -y
 
 # setup bind
 sudo yum -y install bind bind-utils
 
 # set bind files
-sudo cp /etc/named.conf /etc/named.conf.ori
+sudo cp /etc/named.conf /etc/named.conf.$now
 sudo cp /vagrant/cfg_files/bind/named.conf /etc/named.conf
 sudo chown root:named /etc/named.conf
 sudo cp /vagrant/cfg_files/bind/zones/* /var/named/
@@ -20,7 +22,7 @@ sudo /etc/init.d/named restart
 sudo yum -y install ntp
 
 if [ -e /etc/ntp.conf ]; then
-  sudo cp /etc/ntp.conf /etc/ntp.conf.ori
+  sudo cp /etc/ntp.conf /etc/ntp.conf.$now
 fi
 sudo cp /vagrant/cfg_files/ntp/ntp.conf /etc/ntp.conf
 sudo /etc/init.d/ntpd stop
@@ -34,11 +36,11 @@ chkconfig --list
 sudo yum -y install postfix cyrus-sasl
 sudo chkconfig saslauthd on
 if [ -e /etc/postfix/main.cf ]; then
-  sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.ori
+  sudo cp /etc/postfix/main.cf /etc/postfix/main.cf.$now
 fi
 sudo cp /vagrant/cfg_files/postfix/main.cf /etc/postfix/main.cf
 if [ -e /etc/postfix/master.cf ]; then
-  sudo cp /etc/postfix/master.cf /etc/postfix/master.cf.ori
+  sudo cp /etc/postfix/master.cf /etc/postfix/master.cf.$now
 fi
 sudo cp /vagrant/cfg_files/postfix/master.cf /etc/postfix/master.cf
 
@@ -47,6 +49,5 @@ sudo /etc/rc.d/init.d/saslauthd start
 sudo echo unknown_user: /dev/null >> /etc/aliases
 sudo newaliases
 
-now=$(date +"%Y%m%d_%H_%M_%S")
 sudo postconf -d > postfix_setting_$now.log
 sudo /etc/rc.d/init.d/postfix restart
