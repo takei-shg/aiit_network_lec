c7720fe980439ebda12827072f74362316c968a0
takei.shg <takei.shg@gmail.com>
2017-07-18 19:04:18 +0900

    Add vagrantfile and mod provision.sh

--- provision.sh
+++ provision.sh
@@ -7,9 +7,9 @@ sudo yum -y install bind bind-utils
 
 # set bind files
 sudo cp /etc/named.conf /etc/named.conf.ori
-sudo cp /vagrant/aiit_network_lec/cfg_files/bind/named.conf /etc/named.conf
+sudo cp /vagrant/cfg_files/bind/named.conf /etc/named.conf
 sudo chown root:named /etc/named.conf
-sudo cp /vagrant/aiit_network_lec/cfg_files/bind/zones/* /var/named/
+sudo cp /vagrant/cfg_files/bind/zones/* /var/named/
 sudo chown root:named /var/named/net06.*
 sudo /etc/init.d/named restart
 
